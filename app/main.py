0dbb2f7a45ef196377d620dc330213215d323ff1
Katarzyna Rzesikowska <katarzyna.rzesikowska@gmail.com>
2025-02-09 11:24:09 +0100

    add obsidian fixtures

--- app/main.py
+++ app/main.py
@@ -12,10 +12,18 @@ p = yolo(42)
 
 dt.datetime.now()
 
-def add(a: int, b: int) -> Any:  # powinno zwrócic warning
+def add(a: int, b: int):  # powinno zwrócic warning
     return None if a > 42 else 42
 
 
 x: float = cast(float, add(1, 2))
 
 result = x + 42
+
+
+def fields(s):
+     return s.split(',')
+
+def first_field(x: str) -> str:
+    # Error: Returning Any from function declared to return "str"  [no-any-return]
+    return fields(x)[0]
